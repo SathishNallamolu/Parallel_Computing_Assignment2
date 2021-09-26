c9d7ba74ce4f206b28b2140fc9f66c1e7d0d9ab9
Sathish <nallamolusathish56@gmail.com>
2021-09-26 13:18:00 +0530

    first commit

--- CS21M011/p4.c
+++ CS21M011/p4.c
@@ -20,6 +20,7 @@ int main(int argc,char **argv){
 		int sizeof_each_array=n/np;
 		int a[sizeof_each_array];
 
+
 		MPI_Send(&n,1,MPI_INT,1,10,MPI_COMM_WORLD);
 		
 		for(i=0;i<sizeof_each_array;i++){
@@ -27,10 +28,16 @@ int main(int argc,char **argv){
 			sum+=a[i];
 		}
 		
+		// we are seinding the data using the block distribution. here the array is treated as the block.
 		MPI_Send(&i,1,MPI_INT,1,1,MPI_COMM_WORLD);
 		
+		//sending the sum for the next process.
+		// first process sends the array(by usng block distribution) and sum to next process.
 		MPI_Send(&sum,1,MPI_INT,1,0,MPI_COMM_WORLD);
 		
+		//receving the sum of the array from previous process. it is done by cyclic distribution.
+		/* first process(p0) sends sum to next process(p1). then p1 sends to p2 and so on. 
+		finally,the first process receives total sum.(cyclic distribution of data). */
 		MPI_Recv(&sum,1,MPI_INT,size-1,size-1,MPI_COMM_WORLD,&status);
 				
 		printf("SUM:%d\n",sum);
@@ -52,8 +59,10 @@ int main(int argc,char **argv){
 			sum1+=a[j];
 		}	
 		
+		//sending the starting index number of next process.
 		MPI_Send(&j,1,MPI_INT,rank+1,rank+1,MPI_COMM_WORLD);
 		
+		//sending sum to next process in a cyclic way.
 		MPI_Send(&sum1,1,MPI_INT,(rank+1)%size,rank,MPI_COMM_WORLD);			
 	}
 	
